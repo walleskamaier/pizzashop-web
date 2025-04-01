f16ec0953d65e0841973efd5b2c1c85c7389a50e
Walleska Maier <walleskamaier@gmail.com>
2025-03-31 21:21:55 -0300

    construindo página de login

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,7 +1,26 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@radix-ui/react-label";
+
 export function SignIn() {
   return (
-    <div>
-      <h1>SignIn</h1>
+    <div className="p-8">
+      <div className="w-[350px] flex flex-col justify-center gap-6">
+        <div className="flex flex-col gap-2 text-center">
+          <h1 className="text-2xl font-semibold tracking-tight">Acessar painel</h1>
+          <p className="text-sm text-muted-foreground">
+            Acompanhe suas vendas pelo painel do parceiro!
+          </p>
+        </div>
+        <form className="space-y-4">
+          <div className="space-y-2">
+            <Label htmlFor="email">Seu e-mail</Label>
+            <Input id="email" type="email"/>
+
+          </div>
+          <Button className="w-full" type="submit">Acessar painel</Button>
+        </form>
+      </div>
     </div>
-  );
+  )
 }

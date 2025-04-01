4076e66ad7dbfbc9bc71c443b4f1239c65d36c78
Walleska Maier <walleskamaier@gmail.com>
2025-04-01 11:21:25 -0300

    criando notificações toast (sonner)

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import "./global.css";
 
 import { RouterProvider } from "react-router-dom";
+import { Toaster } from "sonner";
 
 import { router } from "./routes";
 
 export function App() {
   return (
-      <RouterProvider router={router} />  
+    <>
+      <Toaster richColors />
+      <RouterProvider router={router} />
+    </>
   );
 }
--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,26 +1,61 @@
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { toast } from "sonner";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@radix-ui/react-label";
 
+const signInForm = z.object({
+  email: z.string().email(),
+});
+
+type SignInForm = z.infer<typeof signInForm>;
+
 export function SignIn() {
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting },
+  } = useForm<SignInForm>();
+
+  async function handleSignIn(data: SignInForm) {
+    try {
+      console.log(data);
+
+      await new Promise((resolve) => setTimeout(resolve, 2000));
+
+      toast.success("Enviamos um link de autenticação para o seu e-mail!", {
+        action: {
+          label: "Reenviar",
+          onClick: () => handleSignIn(data),
+        },
+      });
+    } catch {
+      toast.error("Credencial inválida");
+    }
+  }
+
   return (
     <div className="p-8">
-      <div className="w-[350px] flex flex-col justify-center gap-6">
+      <div className="flex w-[350px] flex-col justify-center gap-6">
         <div className="flex flex-col gap-2 text-center">
-          <h1 className="text-2xl font-semibold tracking-tight">Acessar painel</h1>
-          <p className="text-sm text-muted-foreground">
+          <h1 className="text-2xl font-semibold tracking-tight">
+            Acessar painel
+          </h1>
+          <p className="text-muted-foreground text-sm">
             Acompanhe suas vendas pelo painel do parceiro!
           </p>
         </div>
-        <form className="space-y-4">
+        <form onSubmit={handleSubmit(handleSignIn)} className="space-y-4">
           <div className="space-y-2">
             <Label htmlFor="email">Seu e-mail</Label>
-            <Input id="email" type="email"/>
-
+            <Input id="email" type="email" {...register("email")} />
           </div>
-          <Button className="w-full" type="submit">Acessar painel</Button>
+          <Button disabled={isSubmitting} className="w-full" type="submit">
+            Acessar painel
+          </Button>
         </form>
       </div>
     </div>
-  )
+  );
 }

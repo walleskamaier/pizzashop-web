814763bb43655d0b5ad0a850c2f96db5dacd9d6f
Walleska Maier <walleskamaier@gmail.com>
2025-04-02 18:23:55 -0300

    criando lista de pedidos

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,16 +1,20 @@
 import { createBrowserRouter } from "react-router-dom";
 
-import { AppLayout } from "./_layouts/app";
-import { AuthLayout } from "./_layouts/auth";
+import { AppLayout } from "./pages/_layouts/app";
+import { AuthLayout } from "./pages/_layouts/auth";
 import { Dashboard } from "./pages/app/dashboard";
 import { SignUp } from "./pages/auth/sign-up";
 import { SignIn } from "./pages/auth/sign-in";
+import { Orders } from "./pages/app/orders/orders";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <AppLayout />,
-    children: [{ path: "/", element: <Dashboard /> }],
+    children: [
+      { path: "/", element: <Dashboard /> },
+      { path: "/orders", element: <Orders /> },
+    ],
   },
   {
     path: "/",

cc5f84472abe9e7c897d20185224f86855707822
Walleska Maier <walleskamaier@gmail.com>
2025-04-27 19:21:50 -0300

    configurando página 404

--- src/routes.tsx
+++ src/routes.tsx
@@ -6,11 +6,13 @@ import { Dashboard } from "./pages/app/dashboard/dashboard";
 import { SignUp } from "./pages/auth/sign-up";
 import { SignIn } from "./pages/auth/sign-in";
 import { Orders } from "./pages/app/orders/orders";
+import { NotFound } from "./pages/404";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <AppLayout />,
+    errorElement: <NotFound />,
     children: [
       { path: "/", element: <Dashboard /> },
       { path: "/orders", element: <Orders /> },

16676ddeab6c82beb6c3b134cc089e77d431ae9c
Walleska Maier <walleskamaier@gmail.com>
2025-05-06 18:35:07 -0300

    erros e página 404

--- src/routes.tsx
+++ src/routes.tsx
@@ -7,12 +7,13 @@ import { SignUp } from "./pages/auth/sign-up";
 import { SignIn } from "./pages/auth/sign-in";
 import { Orders } from "./pages/app/orders/orders";
 import { NotFound } from "./pages/404";
+import { Error } from "./pages/error";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <AppLayout />,
-    errorElement: <NotFound />,
+    errorElement: <Error />,
     children: [
       { path: "/", element: <Dashboard /> },
       { path: "/orders", element: <Orders /> },
@@ -25,5 +26,9 @@ export const router = createBrowserRouter([
       { path: "/sign-in", element: <SignIn /> },
       { path: "/sign-up", element: <SignUp /> },
     ],
+  },
+  {
+    path: "*",
+    element: <NotFound />,
   },
 ]);

01b948b7842f47a310933312b6701d316af3c08b
Walleska Maier <walleskamaier@gmail.com>
2025-05-06 21:34:34 -0300

    setup do ambiente de testes

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
 import path from "node:path";
+
 import tailwindcss from "@tailwindcss/vite";
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import type { UserConfig } from "vite";
+import type { InlineConfig } from "vitest/node";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -11,4 +14,11 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  test: {
+    globals: true,
+    setupFiles: ["./test/setup.ts"],
+    environment: "happy-dom",
+  },
+} as UserConfig & {
+  test: InlineConfig;
 });
